7e76dfc3c5dfe5e1623cfb3809e202681d414676
C. Titus Brown <titus@idyll.org>
2013-06-13 13:27:35 +0000

    commented

--- blast/calc-blast-cover.py
+++ blast/calc-blast-cover.py
@@ -1,4 +1,14 @@
 #! /usr/bin/env python
+"""
+Usage:
+
+   calc-blast-cover.py reference.fa query.x.reference.blastn minmatch query.fa
+
+calc-blast-cover calculates the fraction of bases in 'reference.fa' that are
+covered by BLAST matches from 'query.fa', for sequence in 'query.fa' that are
+longer than 'minmatch'.
+"""
+
 import sys
 import blastparser
 
@@ -11,9 +21,11 @@ if len(sys.argv) != 5:
 MIN_SCORE=200
 MIN_QUERY_LEN = int(sys.argv[3])
 
+# load in the query sequences into a list
 query_seqs = set([ record.name for record in screed.open(sys.argv[4]) \
                        if len(record.sequence) >= MIN_QUERY_LEN ])
 
+# create empty lists representing the total number of bases in the reference
 covs = {}
 for n, record in enumerate(screed.open(sys.argv[1])):
     if n % 1000 == 0:
@@ -22,6 +34,8 @@ for n, record in enumerate(screed.open(sys.argv[1])):
 
     covs[record.name] = [0] * len(record.sequence)
 
+# run through the BLAST records in the query, and calculate how much of
+# the reference is covered by the query.
 for n, record in enumerate(blastparser.parse_fp(open(sys.argv[2]))):
     if n % 100 == 0:
         sys.stdout.write('.')
@@ -47,6 +61,7 @@ for n, record in enumerate(blastparser.parse_fp(open(sys.argv[2]))):
 print ''
 
 
+# print out summary statistics for each of the reference.
 coved = 0
 total = 0
 for name in covs:
@@ -55,4 +70,11 @@ for name in covs:
     f = sum(covs[name]) / float(len(covs[name]))
     #print name, sum(covs[name]), len(covs[name]), f
 
-print coved, total, coved / float(total), sys.argv[1], sys.argv[2], MIN_QUERY_LEN
+print 'total bases in reference:', total
+print 'total ref bases covered :', coved
+print 'fraction                :', coved / float(total)
+print 'reference               :', sys.argv[1]
+print 'blast file              :', sys.argv[2]
+print 'query sequences         :', sys.argv[4]
+
+#print coved, total, coved / float(total), sys.argv[1], sys.argv[2], MIN_QUERY_LEN
