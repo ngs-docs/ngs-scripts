b3b90c2d107e319943cf2f19f1f9530dbb162b5c
C. Titus Brown <titus@idyll.org>
2013-09-23 19:25:39 +0000

    fixed gi name handling, for reals

--- blast/blast-to-csv-with-names.py
+++ blast/blast-to-csv-with-names.py
@@ -7,7 +7,7 @@ def load_names(filename):
     d = {}
     for record in screed.open(filename):
         if record.name.startswith('gi|'):
-           ident = record.name.split('|', 2)[2:]
+           ident = record.name.split('|', 2)[2]
         else:
            ident = record.name
         d[ident] = record.description
@@ -30,12 +30,17 @@ output = csv.writer(sys.stdout)
 # parse BLAST records
 print >>sys.stderr, 'parsing BLAST output'
 for record in blastparser.parse_fp(fp):
+    query_name = record.query_name
     for hit in record:
         for match in hit.matches:
-            query_descr = query_db.get(record.query_name, "")
+            if query_name.startswith('gi|'):
+               query_name= query_name.split('|', 2)[2]
+#               print >>sys.stderr, '**', query_descr
+            query_descr = query_db.get(query_name, "")
             against_descr = against_db.get(hit.subject_name, "")
+            
             # output each match as a separate row
-            row = [record.query_name, query_descr,
+            row = [query_name, query_descr,
                    hit.subject_name, against_descr, match.score,
                    match.expect]
             output.writerow(row)
